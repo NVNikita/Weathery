6edbc248af8e4da32c6b3dde37f992814173c0cc
Nikita <nvnagornyi@gmail.com>
2025-08-19 12:12:34 +0300

    Initial Commit

---
+++ Weathery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weathery
+//
+//  Created by Никита Нагорный on 19.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
