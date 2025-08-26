5047d7ab4ad06e608268de0216d70aa72db2ee6d
Nikita <nvnagornyi@gmail.com>
2025-08-26 23:11:20 +0300

    add UIActivityIndicatorView

--- Weathery/WeatheryViewController.swift
+++ Weathery/WeatheryViewController.swift
@@ -13,6 +13,7 @@ final class WeatheryViewController: UIViewController {
     private let temperatureLabel = UILabel()
     private let weatherDescriptionLabel = UILabel()
     private let weatherIcon = UIImageView()
+    private let activityIdicator = UIActivityIndicatorView(style: .large)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,11 +30,13 @@ final class WeatheryViewController: UIViewController {
         view.addSubview(temperatureLabel)
         view.addSubview(weatherDescriptionLabel)
         view.addSubview(weatherIcon)
+        view.addSubview(activityIdicator)
         
         cityLabel.translatesAutoresizingMaskIntoConstraints = false
         temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
         weatherDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         weatherIcon.translatesAutoresizingMaskIntoConstraints = false
+        activityIdicator.translatesAutoresizingMaskIntoConstraints = false
         
         cityLabel.font = .systemFont(ofSize: 34, weight: .light)
         cityLabel.textColor = .white
@@ -46,36 +49,43 @@ final class WeatheryViewController: UIViewController {
         
         weatherIcon.image = UIImage(systemName: "sun.max.fill") // Тест для верстки
         weatherIcon.tintColor = .systemYellow
+        
+        activityIdicator.color = .white
     }
     
     private func setupConsraints() {
         NSLayoutConstraint.activate([
             cityLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            cityLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            cityLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             
             temperatureLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             temperatureLabel.topAnchor.constraint(equalTo: cityLabel.bottomAnchor, constant: 30),
             
             weatherDescriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            weatherDescriptionLabel.topAnchor.constraint(equalTo: temperatureLabel.bottomAnchor),
+            weatherDescriptionLabel.topAnchor.constraint(equalTo: temperatureLabel.bottomAnchor, constant: 5),
             
             weatherIcon.topAnchor.constraint(equalTo: weatherDescriptionLabel.bottomAnchor, constant: 15),
             weatherIcon.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             weatherIcon.widthAnchor.constraint(equalToConstant: 100),
-            weatherIcon.heightAnchor.constraint(equalToConstant: 100)
+            weatherIcon.heightAnchor.constraint(equalToConstant: 100),
+            
+            activityIdicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIdicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
     
     private func getWeather() {
-        NetworkService.shared.getWeatherForCity("Балашиха") { [ weak self ] result in
+        activityIdicator.startAnimating()
+        NetworkService.shared.getWeatherForCity("Новокузнецк") { [ weak self ] result in
             guard let self else { return }
             DispatchQueue.main.async {
                 switch result {
                 case .success(let weather):
                     let cityName = weather.name
-                    let temperature = "\(weather.main.temp)°"
+                    let temperature = "\(Int(weather.main.temp))°"
                     let weatherDescription = weather.weather.first?.description ?? ""
                     self.config(city: cityName, temperature: temperature, weather: weatherDescription)
+                    self.activityIdicator.stopAnimating()
                 case .failure(let error):
                     print("Error: \(error)")
                 }
